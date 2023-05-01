b2b44dfaee924bb2b1bfc431d8ac922e9fb5e7f5
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2023-05-01 14:03:41 +0300

    sds

--- react-pizza/src/components/Categories.jsx
+++ react-pizza/src/components/Categories.jsx
@@ -1,13 +1,20 @@
+import {useState} from "react";
+
 function Categories() {
+  const [activeIndex, setActiveIndex]=useState(0)
+
+  const onClickCategory = () =>{
+    setActiveIndex(0)
+  }
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li onClick={()=> onClickCategory()} className={activeIndex === 0 ? "active": ''}>Все</li>
+        <li  onClick={()=> onClickCategory()} className={activeIndex === 1 ? "active": ''}>Мясные</li>
+        <li onClick={()=> onClickCategory()} className={activeIndex === 2 ? "active": ''}>Вегетарианская</li>
+        <li onClick={()=> onClickCategory()} className={activeIndex === 3 ? "active": ''} >Гриль</li>
+        <li onClick={()=> onClickCategory()} className={activeIndex === 4 ? "active": ''}>Острые</li>
+        <li onClick={()=> onClickCategory()} className={activeIndex === 5 ? "active": ''}>Закрытые</li>
       </ul>
     </div>
   );
